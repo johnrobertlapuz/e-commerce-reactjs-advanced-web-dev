aa9c2a4840c4b128610a89bdb67d764d913c0919
John Robert Lapuz <j.robert.p.lapuz@gmail.com>
2022-04-18 03:14:03 +0800

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,101 @@
+export const storeProducts = [{
+        id: 1,
+        title: "iPhone 13 Pro Max",
+        img: "img/product-1.jpg",
+        price: 1099,
+        company: "Apple",
+        info: "The iPhone 13 Pro and iPhone 13 Pro Max's design is mostly unchanged from its predecessor. However, the rear camera module now covers a larger area due to the larger lenses. The Face ID and camera module on the front display, or notch, is now 20% smaller than previous generations.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Galaxy Flip",
+        img: "img/product-2.jpg",
+        price: 1000,
+        company: "Samsung",
+        info: "A foldable smartphone from Samsung. Introduced in 2020, the Galaxy Z Flip is one of the first devices with a foldable OLED screen. Reminiscent of the flip phone, which was the generation prior to smartphones, the Z Flip has a 12MP camera with 4K video and 256GB of internal storage.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Samsung Note 20 Ultra",
+        img: "img/product-3.jpg",
+        price: 920,
+        company: "Samsung",
+        info: "TheThe Note 20 Ultra is a huge phone with a 6.9-inch screen. It's so big that I want to bring back the term “phablet” just to describe it.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Macbook Pro 16'",
+        img: "img/product-4.png",
+        price: 1899,
+        company: "Apple",
+        info: "MacBook Pro with the incredibly powerful M1 Pro or M1 Max chip. Amazing battery life. And a brilliant Liquid Retina XDR display.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: " ROG Zephyrus G15",
+        img: "img/product-5.jpg",
+        price: 1763,
+        company: "Asus",
+        info: "Power meets portability in the versatile ROG Zephyrus G15, which puts premium Windows 10 Pro gaming in an ultraslim 1.9kg chassis. Performance is fast and smooth with up to an AMD Ryzen™ 9 5900HS CPU and GeForce RTX™ 3080 GPU. Up to a QHD 165Hz/3ms gaming panel balances speed with rich detail to draw you deep into the action. Lush Dolby Atmos audio from six speakers immerses you in surround sound to enrich every entertainment experience.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Galaxy Book Go",
+        img: "img/product-6.jpg",
+        price: 350,
+        company: "Samsung",
+        info: "Galaxy Book Go fits a massive 14-inch screen, 18 hours of battery life, and LTE connectivity on a compact design that's ready to go wherever life takes you.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Galaxy Watch 4",
+        img: "img/product-7.jpg",
+        price: 250,
+        company: "Samsung",
+        info: "Most of us want to know more about ourselves, so we can be the best version of ourselves. That's why we engineered the all-new Galaxy Watch4 to be the companion to your journey towards a healthier you.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "Apple Watch 4",
+        img: "img/product-8.jpg",
+        price: 399,
+        company: "apple",
+        info: "Apple Watch Series 4 is more than an evolution — it represents a fundamental redesign and re-engineering of Apple Watch. It’s offered in two sizes, 40mm and 44mm. The speaker is 50 percent louder, optimized for phone calls, Siri and Walkie-Talkie, and the microphone has been relocated, to reduce echo for better sound quality. The device includes the next-generation S4 chip with a custom 64-bit dual-core processor, delivering twice the speed while maintaining the same all-day battery life.",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+];
+
+export const detailProduct = {
+    id: 1,
+    title: " ",
+    img: "img/product-1.png",
+    price: 10,
+    company: "Apple",
+    info: "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+};
